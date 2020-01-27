0f077d4edad1fa280ab2097dde03b1d42970230f
Lindsay Mecher <lindsaymecher@gmail.com>
2020-01-26 22:41:05 -0600

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,24 +1,29 @@
 class Application
 
-  @@items = []
+  @@items = [Item.new("item_one", "3.45"), Item.new("item_two", "5.00")]
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path == "/items/<ITEM NAME>"
-      requested_item = req.params["item"]
-      if @@items.include?(requested_item)
-      resp.write "#{requested_item} costs ${reqested_item.price}.\n"
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if item = @@items.find{|i| i.name == item_name}
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
       end
     else
-      resp.write "Route Not Found"
+      resp.write "Route not found"
       resp.status = 404
     end
     resp.finish
   end
 
 end
+
+
 
 #   1. Your application should only accept the `/items/<ITEM NAME>` route. Everything else should `404`
 #   2. If a user requests `/items/<Item Name>` it should return the price of that item
